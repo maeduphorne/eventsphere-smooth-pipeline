f9a36b7ae941e0524c25ac10a1b1ba8973da062d
maeduphorne <maelduphorne@gmail.com>
2024-12-03 21:26:33 -0500

    Customaize app for EventSphere

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,3 +1,8 @@
+test('Hello EventSphere endpoint', () => {
+    const response = 'Hello, EventSphere!';
+    expect(response).toBe('Hello, EventSphere!');
+});  
+
 test('adds 1 + 2 to equal 3', () => {
     const sum = (a, b) => a + b;
     expect(sum(1, 2)).toBe(3);
